6ecd934bf2b128fe08ef6d0b598882aea0df0c04
Virgil Uwaoma <virgiluwaoma@gmail.com>
2022-06-03 12:39:12 +0100

    App and db configuration setup

--- backend/models.py
+++ backend/models.py
@@ -1,78 +1,78 @@
+'''binds a flask application and a SQLAlchemy service'''
 import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+DATABASE_NAME = "trivia"
+DATABASE_PATH = "postgresql://{}/{}".format('localhost:5432', DATABASE_NAME)
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+
+def setup_db(app, database_path=DATABASE_PATH):
+    '''db configurations'''
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_PATH
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-'''
-Question
 
-'''
-class Question(db.Model):  
-  __tablename__ = 'questions'
+class Question(db.Model):
+    '''defines quesrtions model'''
+    __tablename__ = 'questions'
 
-  id = Column(Integer, primary_key=True)
-  question = Column(String)
-  answer = Column(String)
-  category = Column(String)
-  difficulty = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    category = Column(String)
+    difficulty = Column(Integer)
 
-  def __init__(self, question, answer, category, difficulty):
-    self.question = question
-    self.answer = answer
-    self.category = category
-    self.difficulty = difficulty
+    def __init__(self, question, answer, category, difficulty):
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        '''insert helper function'''
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        '''update helper function'''
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'question': self.question,
-      'answer': self.answer,
-      'category': self.category,
-      'difficulty': self.difficulty
-    }
+    def delete(self):
+        '''delete helper function'''
+        db.session.delete(self)
+        db.session.commit()
 
-'''
-Category
+    def format(self):
+        '''format helper function'''
+        return {
+          'id': self.id,
+          'question': self.question,
+          'answer': self.answer,
+          'category': self.category,
+          'difficulty': self.difficulty
+        }
 
-'''
-class Category(db.Model):  
-  __tablename__ = 'categories'
 
-  id = Column(Integer, primary_key=True)
-  type = Column(String)
+class Category(db.Model):
+    '''defines category model'''
+    __tablename__ = 'categories'
 
-  def __init__(self, type):
-    self.type = type
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
 
-  def format(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    def __init__(self, type):
+        self.type = type
+
+    def format(self):
+        '''format helper function'''
+        return {
+          'id': self.id,
+          'type': self.type
+        }
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -2,7 +2,6 @@ import os
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
-
 from flaskr import create_app
 from models import setup_db, Question, Category
 
@@ -15,16 +14,16 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+                             'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
-            # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +32,7 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def 
 
 
 # Make the tests conveniently executable
